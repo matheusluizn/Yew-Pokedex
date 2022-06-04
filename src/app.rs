03c1360d7cf14183d4b3bf6557fdbc3bb3161499
mt <matheusluizn@gmail.com>
2022-06-04 19:38:53 -0300

    chore: rollback to yew 0.18

--- src/app.rs
+++ src/app.rs
@@ -11,11 +11,22 @@ impl Component for App{
     type Message = ();
     type Properties = ();
 
-    fn create(_ctx: &Context<Self>) -> Self{
+    fn create(_: Self::Properties, link: ComponentLink<Self> ) -> Self{
         Self {}
     }
+
+    fn update(&mut self, msg: Self::Message) -> ShouldRender {
+        false
+    }
+
+    fn change(&mut self, _props: Self::Properties) -> ShouldRender {
+        // Should only return "true" if new properties are different to
+        // previously received properties.
+        // This component has no properties so we will always return "false".
+        false
+    }
     
-    fn view(&self, _ctx: &Context<Self>) -> Html{
+    fn view(&self) -> Html{
         log::info!("Mammamia");
         html!{
             <>
